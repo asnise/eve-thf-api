e8e9537871eb9f83af1178aeac51da3d4808598c
Asnise_Git <55057145+asnise@users.noreply.github.com>
2021-06-20 16:45:23 +0700

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-var request = new XMLHttpRequest();
+/*var request = new XMLHttpRequest();
 request.open('GET', 'https://api.eve-echoes-market.com/market-stats/stats.csv', true);
 var data_call= {};
 request.send();
@@ -22,40 +22,31 @@ request.onload = function(){
       }
    }
    console.log(data_call);
-   document.getElementById("dvCSV").innerHTML = JSON.stringify(data_call, null, 4);
+   let text = '<select>'
+   for (let x in data_call) {
+     text += "<option>" + data_call[x].name;
+   }
+   text += "</select>"
+   document.getElementById("dvCSV").innerHTML = text;
+   
  }
 };
+*/
 
-
-/*$(document).ready(function(){
+var data_load;
+var data_load_body;
+$(document).ready(function(){
    $.ajax({
-     url: 'https://api.eve-echoes-market.com/market-stats/stats.csv' ,
-     format: '',
-     dataType: "",
-     success(data){
-        var rows;
-        rows = data.split("\r\n");
-        for (var i = 0; i < rows.length; i++) {
-           var cells = rows[i].split(",");
-           if (cells.length > 1) {
-              var data_call = {};
-              data_call[cells[1].replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "").replace(" ", "")] = {
-                 id: cells[0],
-                 name: cells[1],
-                 time: cells[2],
-                 sell: cells[3],
-                 buy: cells[4],
-                 lowest_sell: cells[5],
-                 highest_buy: cells[6]
-              }
-              data_call.push(data_call);
-           }
-        }
-        obj = JSON.stringify(data_call);
-        document.getElementById("dvCSV").innerHTML = obj;
-     }
+     url: 'https://raw.githubusercontent.com/asnise/eve-thf-api/main/data.json' ,
+     format: 'json',
+     dataType: "json",
+  success(data){
+	  data_load = data;
+     let vers = $('<h4>').text(data);
+     $('#dvCSV').append(vers);
+  }
    });
-});*/
+});
 
 
 
